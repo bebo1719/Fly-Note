744f61285722b1bcf095391afa468fbb36654ce6
bebo1719 <142172190+bebo1719@users.noreply.github.com>
2024-10-25 18:25:39 -0500

    .

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
